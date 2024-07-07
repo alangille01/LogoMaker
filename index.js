332107075d698e97ffadf213de3089f528c3b744
Alexis Langille <langille.alexis@gmail.com>
2024-07-07 14:13:27 -0400

    Create SVG File based on Prompt Input

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// https://www.npmjs.com/package/validate-color
-var validateColor = require("validate-color").default;
+const commandLineQuestions = require('./lib/questions');
+const generateSVG = require('./lib/shapes');
 
-
-const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters:',
-        validate: input => input.length <= 3 ? true : 'You can enter up to three characters only.'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        validate: input => validateColor(input) ? true : "Invalid text colour chosen"
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape:',
-        choices: ['circle', 'triangle', 'square']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter the shape\'s color (color keyword or hexadecimal number):',
-        validate: input => validateColor(input) ? true : "Invalid colour chosen"
-    }
-];
-
-inquirer.prompt(questions).then(answers => {
+inquirer.prompt(commandLineQuestions).then(answers => {
+    const svgContent = generateSVG(answers);
+    fs.writeFileSync('logo.svg', svgContent);
     console.log('Generated logo.svg');
 });
